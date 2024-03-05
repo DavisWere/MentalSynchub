6d2370460b2722140e6cb6488e10fc1417bd60f0
DavisWere <devisodhis10@gmail.com>
2024-03-05 08:37:13 +0300

    add model views

--- core/views.py
+++ core/views.py
@@ -11,6 +11,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import generics
 from django.http import JsonResponse
+import random
 from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt 
 from django.db import transaction
@@ -20,8 +21,9 @@ from django.conf import settings
 from rest_framework.response import Response
 from django.views import View
 from rest_framework_simplejwt.views import TokenObtainPairView
-from core.models import( User, ChatCompletion)
-from core.serializers import ( CustomTokenObtainPairSerializer,ChatCompletionSerializer, UserSerializer)
+from core.models import( User, BookingSession, Transaction, Game, ChatCompletion)
+from core.serializers import ( CustomTokenObtainPairSerializer,ChatCompletionSerializer, 
+                              UserSerializer, BookingSessionSerializer, GameSerializer, TransactionSerializer)
 
 class CustomObtainTokenPairView(TokenObtainPairView):
     permission_classes = [permissions.AllowAny]
@@ -31,7 +33,88 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset= User.objects.all()
     permission_classes = [permissions.IsAuthenticated]
+    
 
+class BookingSessionViewSet(viewsets.ModelViewSet):
+    serializer_class = BookingSessionSerializer
+    queryset = BookingSession.objects.all()
+    permission_classes= [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user =self.request.user
+        all_booked_sessions = BookingSession.objects.filter(user=user)
+        return all_booked_sessions
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GameView(APIView):
+    queryset =Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+        
+   
+    def post(self, request):
+        player_name = request.user.username
+        if not player_name:
+            return Response({'error': 'Player name is required'}, status=400)
+
+        user_input = request.data.get('player_choice')
+        if user_input not in ["rock", "paper", "scissors"]:
+            return Response({'error': 'Invalid choice'}, status=400)
+        
+        computer_choice = random.choice(["rock", "paper", "scissors"])
+        
+        user_score, computer_score, result = self.determine_winner(user_input, computer_choice)
+
+        # Create a new game instance
+        game = Game.objects.create(
+            user=request.user,
+            player_choice=user_input,
+            computer_choice=computer_choice,
+            result=result
+        )
+
+        # If the user wants to play again, return the game result without calculating the total score
+        play_again = request.data.get('play_again', False)
+        if play_again:
+            return Response({
+                'player_name': player_name,
+                'user_choice': user_input,
+                'computer_choice': computer_choice,
+                'result': result,
+                'user_score': user_score,
+                'computer_score': computer_score,
+            })
+
+        # If play_again is False, calculate the total score by summing up the scores of all games
+        games = Game.objects.filter(user=request.user)
+        total_user_score = sum(game.user_score for game in games)
+        total_computer_score = sum(game.computer_score for game in games)
+
+        # Return the total scores along with the last game result
+        return Response({
+            'player_name': player_name,
+            'user_choice': user_input,
+            'computer_choice': computer_choice,
+            'result': result,
+            'total_user_score': total_user_score,
+            'total_computer_score': total_computer_score,
+        })
+    
+    def determine_winner(self, user_input, computer_choice):
+        if user_input == computer_choice:
+            return 0.5, 0.5, "A Draw!"
+        elif (user_input == "rock" and computer_choice == "scissors") or \
+             (user_input == "paper" and computer_choice == "rock") or \
+             (user_input == "scissors" and computer_choice == "paper"):
+            return 1, 0, "You Won!"
+        else:
+            return 0, 1, "You lost!"
 
 
 class ChatCompletion(View):
