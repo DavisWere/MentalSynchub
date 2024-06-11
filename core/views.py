91ad80155eb62a8791db05639e30f4d350f55dca
ColinAdo <colinadodev30@gmail.com>
2024-06-11 22:34:51 +0300

    Removed unnecessary imports

--- core/views.py
+++ core/views.py
@@ -3,27 +3,13 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-
-
-from django.shortcuts import render
-import calendar
-from django.utils import timezone
-import requests
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework import generics
-from django.http import JsonResponse
+
 import random
-from rest_framework.decorators import api_view
-from django.views.decorators.csrf import csrf_exempt
-from django.db import transaction
 import os
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
 from django.conf import settings
 from rest_framework.response import Response
 from django.views import View
