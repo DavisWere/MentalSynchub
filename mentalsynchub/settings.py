2a00a91104910b78fce43a35eeac356fda22d054
DavisWere <devisodhis10@gmail.com>
2024-07-04 14:55:28 +0300

    "Updated ALLOWED_HOSTS and CORS settings to include new domains"

--- mentalsynchub/settings.py
+++ mentalsynchub/settings.py
@@ -32,7 +32,7 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT')
 DEBUG = False if ENVIRONMENT != 'dev' else True
 
 ALLOWED_HOSTS = os.getenv(
-    'ALLOWED_HOST', 'http://localhost:5173,localhost,127.0.0.1,openup.up.railway.app').split(',')
+    'ALLOWED_HOST', 'http://localhost:5173,localhost,127.0.0.1,https://web-openup.netlify.app/,openup.up.railway.app').split(',')
 
 
 # Application definition
@@ -70,14 +70,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mentalsynchub.urls'
-CORS_ALLOW_ALL_ORIGINS = ['*']
-# CORS_ALLOWED_ORIGINS = ['https://mentalsynchub.netlify.app',
-#                         'https://e43c-102-0-4-206.ngrok-free.app',
-#                         'http://localhost:3000']
+# CORS_ALLOW_ALL_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = ['https://mentalsynchub.netlify.app',
+                        'https://e43c-102-0-4-206.ngrok-free.app',
+                        'http://localhost:3000', 'https://web-openup.netlify.app/']
 CORS_ALLOW_CREDENTIALS = False
-# CSRF_TRUSTED_ORIGIN = [
-#     'https://openup.up.railway.app', 'http://localhost:3000']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGIN = [
+    'https://openup.up.railway.app', 'http://localhost:3000', 'https://web-openup.netlify.app/']
+# CSRF_TRUSTED_ORIGINS = ['*']
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
